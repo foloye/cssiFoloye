9a669adb5959a2b896aaddf3776eb1378e4a7172
Glitch (cssi-day8-foloye-cal) <none>
2020-10-01 05:30:12 +0000

    👵🐚 Updated with Glitch

--- script.js
+++ script.js
@@ -1,38 +1,68 @@
-/* global createCanvas, background, ellipse, rect, noFill, strokeWeight, stroke */
-// Name any p5.js functions we use in the global so Glitch can recognize them.
-// Add to this list as you consult the p5.js documentation for other functions. 
+// Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
+// Add to this list as you consult the p5.js documentation for other functions.
 /* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
-          rect, ellipse, stroke, image, loadImage, collideCircleCircle, text, mouseX, mouseY, 
-          strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, noFill,
-          triangle, quad,square, circle */
+          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
+          mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
+          keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
+let drops;
 
 
 function setup() {
-  // Code here runs only once
-  createCanvas(800, 600);
+  createCanvas(500, 500);
+  colorMode(HSB, 100);
+  // Variables for droplet 1
+  let numDots = 25;
+  drops = [];
+  for (let i = 0; i < numDots; i++){
+    drops.push(new RainDrop);
+  }
+  
 }
 
 function draw() {
-  // Code here runs continuously
-  background(255,255,255);
+  background(0, 0, 95);
+  for (let i = 0; i < drops.length; i++){
+    drops[i].dropAndShow();
+  }
   
-  stroke('red');
-  strokeWeight(20);
-  circle(80,80,50,50);
-  
-  circle(80,80, 10);
-  
-  stroke(242, 81, 34);
-  square(50, 200, 12);
-  
-  stroke(93, 188, 210);
-  square(50, 234, 12);
-  
-  stroke(126, 187, 0);
-  square(84, 200, 12);
-  
-  stroke(255, 185, 1);
-  square(84, 234, 12);
+}
+class RainDrop {
+  // the constructor will be called wheneeveer you run 'new RainDrop()'
+  constructor(x, y, d, fallSpeed) {
+    this.x = random(width);
+    this.y = 0;
+    this.d = random(10,20);
+    this.fallSpeed = random(5,15);
+  }
+// class Grass {
+//   constructor ()
+// }
 
+
+  show() {
+    noStroke();
+    fill(60, 80, 80);
+    ellipse(this.x, this.y, this.d);
+  }
+  grass(){
+    
+  }
+
+  drip() {
+    this.y += this.fallSpeed;
+    // If it goes off the screen...
+    if (this.y > height) {
+      // ...reset it...
+      this.y = 0;
+      // ...and move it somewhere random.
+      this.x = random(width);
+    }
+  }
+    
+  dropAndShow(){
+    this.drip();
+    this.show();
+  }
+  
 }
