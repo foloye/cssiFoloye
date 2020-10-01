978e5dc0e70b1316d5ea454771de9798e33924da
Glitch (cssi-day10-p) <none>
2020-10-01 05:24:29 +0000

    💓👩‍👩‍👦 Updated with Glitch

--- script.js
+++ script.js
@@ -1,424 +1,173 @@
-/* global createCanvas, colorMode, HSB, width, height, random, round, background, fill, noFill, color, random,
-          rect, ellipse, stroke, image, loadImage, frameRate, text, sound, p5, textAlign, BOTTOM,LEFT,
-          mouseX,key, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
-          keyCode,save, textSize, noLoop, loop, createAudio, play, textStyle, BOLD, userStartAudio */
+// Name any p5.js functions we use in the global so Glitch can recognize them.
+// Add to this list as you consult the p5.js documentation for other functions.
+/* global createCanvas,keyCode, floor, colorMode, HSB, width, height, random, background, fill, color, random,
+          rect, ellipse, stroke, text, mouseX, mouseY, 
+          strokeWeight,clear, createSlider, line,removeItem,  mouseIsPressed, windowWidth, windowHeight, noStroke, keyIsDown, CONTROL, textSize, loadImage, image, pmouseX, pmouseY, createAudio */
 
-let backgroundImage,
-  keys,
-  cNote,
-  cSharp,
-  dNote,
-  dSharp,
-  eNote,
-  fNote,
-  fSharp,
-  gNote,
-  gSharp,
-  aNote,
-  aSharp,
-  bNote,
-  highcNote,
-  soundList,
-  assignSound,
-  assignColor,
-  sheetMusicOne,
-  sheetMusicTwo,
-  tutorialTrack,
-  tutorialStarted,
-  tutorialPicked,
-  blackKeyExists;
+//modified from kmarventano@ for Google CSSI
 
-let mic, recorder, soundFile;
-let recordingText = 0;
-let ellipseOpacity = 75;
-let state = 0;
+let brushHue,
+  priorX,
+  priorY,
+  color1,
+  globalS,
+  globalB,
+  backgroundColor,
+  textColor,
+  playersWord,
+  jars,
+  painter,
+  eraser,
+  slider,
+  music;
+
+let randomWords = [
+  "fries",
+  "book",
+  "dog",
+  "cat",
+  "phone",
+  "apple",
+  "banana",
+  "butterfly",
+  "pig",
+  "cactus"
+];
 
 function setup() {
-  // canvas and color settings
-  createCanvas(800, 600);
-  colorMode(HSB, 360, 100, 100);
-  backgroundImage = loadImage(
-    `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2Fbackground.jpg?v=1596129602980`
-  );
-  // arrays for the piano keys, sounds, and notes for twinkle twinkle
-  keys = [];
-  soundList = [];
-  sheetMusicOne = [
-    0,
-    0,
-    4,
-    4,
-    4,
-    5,
-    5,
-    4,
-    3,
-    3,
-    2,
-    2,
-    1,
-    1,
-    1,
-    0,
-    4,
-    4,
-    3,
-    3,
-    2,
-    2,
-    1,
-    4,
-    4,
-    3,
-    3,
-    2,
-    2,
-    1,
-    0,
-    0,
-    4,
-    4,
-    5,
-    5,
-    4,
-    3,
-    3,
-    2,
-    2,
-    1,
-    1,
-    0
-  ];
-  sheetMusicTwo = [2, 1, 0, 2, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 1, 0];
-  // starter variables for identifying the tutorial
-  tutorialTrack = 0;
-  tutorialStarted = false;
-  // add the keys into the array
-  let whiteKeyCount = 0;
-  for (let i = 0; i < 8; i++) {
-    //appends new white keys to keys array
-    keys.push(new PianoKeys(whiteKeyCount, "white"));
-    whiteKeyCount += 100;
-  }
-  let blackKeyCount = 75;
-  for (let i = 0; i < 5; i++) {
-    //appends black keys to keys array
-    keys.push(new PianoKeys(blackKeyCount, "black"));
-    if (i == 1) {
-      blackKeyCount += 200;
-    } else {
-      blackKeyCount += 100;
-    }
-  }
-  //creates the sound of each key and adds it to soundList array
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FC.mp3?v=1595874627030`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FD%20(online-audio-converter.com).mp3?v=1595877615674`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FE.mp3?v=1595877621734`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FF.mp3?v=1595877624520`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FG.mp3?v=1595877630881`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FA.mp3?v=1595877636191`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FB.mp3?v=1595877640843`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FHighc.mp3?v=1595877647695`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FCsharp.mp3?v=1595877607777`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FD%20sharp.mp3?v=1595877618487`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FFsharp.mp3?v=1595877626443`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FGsharp.mp3?v=1595877633169`
-    )
-  );
-  soundList.push(
-    createAudio(
-      `https://cdn.glitch.com/8cfcdbf5-d52d-4f68-9425-525d24565e2a%2FAsharp.mp3?v=1595877638158`
-    )
-  );
-
-  mic = new p5.AudioIn();
-  mic.start();
-  recorder = new p5.SoundRecorder();
-
-  recorder.setInput(mic);
-
-  soundFile = new p5.SoundFile();
+  // Canvas & color settings
+  resetCanvas();
+  
+  playersWord = getWord();
+  
+  jars = [new paintJar(50, color1), new paintJar(110, color(240, 100, 100)), new paintJar(170, color(60, 100, 100)), 
+          new paintJar(230, color(200, 0, 75))];
 }
 
 function draw() {
-  background(backgroundImage);
-  //shows the piano keys on the screen
-  for (let i = 0; i < keys.length; i++) {
-    keys[i].show();
+  printInstructions();
+
+  writeWord(playersWord);
+  //painting = image(painter,mouseX - 30, mouseY - 20, 50, 50);
+
+  //creates a redJar object for the red "paint jar",
+  //shows the new object, use the getColor() method for the new object
+  for (let i = 0; i < jars.length; i++) {
+    jars[i].show();
+    jars[i].getColor();
   }
-  // tells user which key is which
-  fill("gray");
-  textSize(40);
-  text("C", 35, 400);
-  text("D", 135, 400);
-  text("E", 235, 400);
-  text("F", 335, 400);
-  text("G", 435, 400);
-  text("A", 535, 400);
-  text("B", 635, 400);
-  text("C", 735, 400);
-  textSize(15);
-  text("C#/Db", 80, 250);
-  text("D#/Eb", 180, 250);
-  text("F#/Gb", 380, 250);
-  text("G#/Ab", 480, 250);
-  text("A#/Bb", 580, 250);
-  // the tutorial buttons
-  stroke("SaddleBrown");
-  fill("Bisque");
-  rect(50, 20, 80, 40);
-  rect(500, 20, 80, 40);
-  fill("SaddleBrown");
+
+  image(eraser, 205, 20, 60, 60);
+
+  //TODO 3: create two more jars of color
+
+  if (mouseIsPressed) {
+    //TODO 4: can you think of a way to use line() instead of ellipse()?
+    strokeWeight(slider.value())
+    line(pmouseX, pmouseY, mouseX, mouseY); 
+  }
+  strokeWeight(1)
+
+  priorX = mouseX;
+  priorY = mouseY;
+  
+ //music = createAudio(`https://cdn.glitch.com/aad08a6d-d68f-4b3c-bef9-8a64a6db3909%2F1%20Minute%20Timer%20with%20Music%20for%20Kids!%20One%20Minute%20Calming%20Meditation%20Relaxing%20Music!.mp3?v=1595012339676`);
+ //music.autoplay(true);
+  //music is distorted no matter what
+}
+
+function printInstructions() {  
+  //TODO 5: printInstructions should display instructions for how to hide the word
+  //and anything else users might need to know
+  fill(0);
   textSize(20);
-  text("Tutorial", 55, 45);
-  text("Tutorial", 505, 45);
-  fill("white");
-  text("(Twinkle Twinkle Little Star)", 140, 45);
-  text("(Hot Cross Buns)", 590, 45);
-  textSize(15);
-  stroke("White");
-  fill("Black");
-  if (recordingText == 0) {
-    //textAlign(LEFT, BOTTOM);
-    textStyle(BOLD);
-    text(
-      "Double click the green square to begin recording!",
-      width / 2 - 10,
-      height - 15
-    );
-  } else if (recordingText == 1) {
-    text(
-      "Recording in Progress, Double click the green\n square to stop recording.",
-      width / 2 - 5,
-      height - 30
-    );
-    
-    
-  } else {
-    text("Done! Double Click to Download and \n play your recording!", width / 2, height - 30);
-  }
-  fill(120,100,ellipseOpacity);
-  stroke(120,100,ellipseOpacity);
-  rect(width - 50, height - 50, 50, 50);
+  //strokeWeight(0);
+  text("1. Use mouse to paint", 80, height - 50);
+  text("2. Press any key to restart", 70, height - 30);
+  text("3. Press the spacebar for a new word.", 30, height-10);
 }
 
-function mousePressed() {
-  blackKeyExists = false;
-  //Match the sound to the according BLACK key
-  for (let i = 8; i < keys.length; i++) {
-    if (
-      mouseX > keys[i].xPos &&
-      mouseX < keys[i].xPos + 50 &&
-      mouseY > 100 &&
-      mouseY < 300
-    ) {
-      assignSound = soundList[i];
-      //changes the color of key that has been pressed
-      if (!tutorialStarted) {
-        keys[i].color = "DeepSkyBlue";
-      }
-      assignColor = i;
-      blackKeyExists = true;
-      //plays note designated to key that was pressed
-      assignSound.play();
-    }
-  }
-  //Match the sound to the according WHITE key (if mouse is not on black key)
-  if (!blackKeyExists) {
-    for (let i = 0; i < 8; i++) {
-      if (
-        mouseX > keys[i].xPos &&
-        mouseX < keys[i].xPos + 100 &&
-        mouseY > 100 &&
-        mouseY < 500
-      ) {
-        assignSound = soundList[i];
-        //changes the color of key that has been pressed
-        if (!tutorialStarted) {
-          keys[i].color = "DeepSkyBlue";
-        }
-        assignColor = i;
-        //plays note designated to key that was pressed
-        assignSound.play();
-      }
-    }
-  }
-  // begins tutorial if button is pressed
-  if (mouseX > 50 && mouseX < 130 && mouseY > 20 && mouseY < 60) {
-    if (tutorialStarted == true) {
-      for (let i = 0; i < 8; i++) {
-        keys[i].color = "white";
-      }
-    }
-    tutorialTrack = 0;
-    tutorialStarted = true;
-    tutorialPicked = 1;
-    tutorial(tutorialTrack);
-  }
-  if (mouseX > 500 && mouseX < 580 && mouseY > 20 && mouseY < 60) {
-    if (tutorialStarted == true) {
-      for (let i = 0; i < 8; i++) {
-        keys[i].color = "white";
-      }
-    }
-    tutorialTrack = 0;
-    tutorialStarted = true;
-    tutorialPicked = 2;
-    tutorial(tutorialTrack);
-  }
+function getWord() {
+  //TODO 1: getWord should return a single random word from an array of words
+  // let randomWords = ["fries", "book", "dog", "cat", "phone","apple", "banana","butterfly","pig", "cactus"];
+
+  return randomWords[floor(random(randomWords.length))];
 }
 
-function mouseReleased() {
-  // when key is released, sound stops
-  assignSound.stop();
-  if (assignColor > 7 && !tutorialStarted) {
-    //changes key to its original color
-    keys[assignColor].color = "black";
-    keys[assignColor].show();
-  } else if (!tutorialStarted) {
-    keys[assignColor].color = "white";
-  }
-  // continues tutorial unless sheet music has ended
-  if (
-    !blackKeyExists &&
-    mouseX > keys[sheetMusicOne[tutorialTrack]].xPos &&
-    mouseX < keys[sheetMusicOne[tutorialTrack]].xPos + 100 &&
-    mouseY > keys[sheetMusicOne[tutorialTrack]].yPos &&
-    mouseY < keys[sheetMusicOne[tutorialTrack]].yPos + 400 &&
-    tutorialStarted == true
-  ) {
-    tutorial(tutorialTrack + 1);
-    if (tutorialTrack == sheetMusicOne.length - 2) {
-      tutorialStarted = false;
-    } else {
-      tutorialTrack++;
-    }
-  }
-  if (
-    !blackKeyExists &&
-    mouseX > keys[sheetMusicTwo[tutorialTrack]].xPos &&
-    mouseX < keys[sheetMusicTwo[tutorialTrack]].xPos + 100 &&
-    mouseY > keys[sheetMusicTwo[tutorialTrack]].yPos &&
-    mouseY < keys[sheetMusicTwo[tutorialTrack]].yPos + 400 &&
-    tutorialStarted == true
-  ) {
-    tutorial(tutorialTrack + 1);
-    if (tutorialTrack == sheetMusicOne.length - 2) {
-      tutorialStarted = false;
-    } else {
-      tutorialTrack++;
-    }
-  }
+function writeWord(word) {
+  noStroke();
+  textSize(13);
+  fill(textColor);
+  text("Your word: " + word, 290, 25);
 }
 
-// shows the next step of the tutorial in dark blue
-function tutorial(tutorialTrack) {
-  if (tutorialPicked == 1) {
-    if (tutorialTrack > 0) {
-      keys[sheetMusicOne[tutorialTrack - 1]].color = "white";
-    }
-    keys[sheetMusicOne[tutorialTrack]].color = "DarkBlue";
-  } else if (tutorialPicked == 2) {
-    if (tutorialTrack > 0) {
-      keys[sheetMusicTwo[tutorialTrack - 1]].color = "white";
-    }
-    keys[sheetMusicTwo[tutorialTrack]].color = "DarkBlue";
-  }
-}
-
-class PianoKeys {
+class paintJar {
   constructor(xPos, color) {
     this.xPos = xPos;
-    this.color = color;
-    // if statement to make the piano keys their correct size
-    if (color == "black") {
-      this.pianoH = 200;
-      this.pianoW = 50;
-    } else if (color == "white") {
-      this.pianoH = 400;
-      this.pianoW = 100;
+    this.HSBColor = color;
+    this.yPos = 50;
+    this.radius = 50;
+  }
+
+  getColor() {
+    if (
+      mouseX > this.xPos - this.radius / 2 &&
+      mouseX < this.xPos + this.radius / 2 &&
+      mouseY > this.yPos - this.radius / 2 &&
+      mouseY < this.yPos + this.radius / 2 &&
+      mouseIsPressed
+    ) {
+      brushHue = this.HSBColor;
     }
-    this.yPos = 100;
+
+    stroke(brushHue, 50, 80);
+    fill(brushHue, 50, 80);
   }
 
   show() {
-    //draws the piano keys
-    stroke("black");
-    fill(this.color);
-    rect(this.xPos, this.yPos, this.pianoW, this.pianoH);
+    stroke(this.HSBColor);
+    fill(this.HSBColor);
+    ellipse(this.xPos, this.yPos, this.radius);
   }
 }
 
-function doubleClicked() {
-  if (mouseX >= width - 50 && mouseY >= height - 50 && doubleClicked) {
-    //let recordingIp = false;
-    //if (key === "r") {
-    recordingText++;
-    userStartAudio();
-
-    if (state === 0 && mic.enabled) {
-      recorder.record(soundFile);
-
-      text("Recording!", 0, height - 50);
-
-      state++;
-    } else if (state === 1) {
-      recorder.stop();
-      recordingText++;
-
-      state++;
-    } else if (state === 2) {
-      recordingText++;
-      soundFile.play();
-      save(soundFile, "myPianoSong.wav");
-
-      state++;
-    }
+function keyPressed() {
+  //TODO 2: if a key is pressed, playersWord should be assigned to an empty string ""
+  //and the canvas should be cleared
+  
+  playersWord = "";
+  
+  clear();
+  resetCanvas();
+  if (keyCode == 32){
+    playersWord = getWord();
   }
+  //setup();
+  //draw();
+  //color(200, 0, 75)
+  
+}
+function resetCanvas(){
+  createCanvas(400, 400);
+  colorMode(HSB, 360, 100, 100); //using HSB colors
+  brushHue = 0;
+  priorX = 0;
+  priorY = 0;
+  background(95);
+  strokeWeight(6);
+  painter = loadImage(
+    "https://cdn.glitch.com/aad08a6d-d68f-4b3c-bef9-8a64a6db3909%2Fpaintbrush.png?v=1595006895262"
+  );
+  eraser = loadImage("https://cdn.glitch.com/aad08a6d-d68f-4b3c-bef9-8a64a6db3909%2Fimageedit_1_4453802937.png?v=1595008636089");
+
+  globalS = 90;
+  globalB = 90;
+  backgroundColor = color(200, 0, 75);
+  color1 = color(0, globalS, globalB); //red
+  textColor = color(300, globalS, globalB);
+  background(backgroundColor);
+  
+  slider = createSlider(1, 50, 1);
+  slider.position(0, 0, 100, height);
+  slider.style('width', '80px');
 }
