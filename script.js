cdfc5f00d55d640d42d445032d98c5c9181a9614
Glitch (cssi-day6-foloye-cal) <none>
2020-10-01 05:27:10 +0000

    📻👰 Updated with Glitch

--- script.js
+++ script.js
@@ -1,45 +1,80 @@
-/* global createCanvas, background, loadImage, image, width, height */
+// Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
+// Add to this list as you consult the p5.js documentation for other functions.
+/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
+          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
+          mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
+          keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
 
-let dvdImage, x, xVelocity, y, yVelocity, logoWidth, logoHeight;
+let brushHue,addTimeC,aTCX,aTCY, backgroundColor,coinW, coin2X,coin2Y,coinW2, coinX, coinY, score, time, gameIsOver, hit, hit2;
 
-function setup(){
-  createCanvas(800, 600);
-  // We only want to load the logo once.
-  dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
-  x = 50;
-  xVelocity = 1;
-  y = 50;
-  yVelocity = 1;
-  logoWidth = 200;
-  logoHeight = 150;
+function setup() {
+  // Canvas & color settings
+  createCanvas(400, 400);
+  colorMode(HSB, 360, 100, 100);
+  brushHue = 0;
+  backgroundColor = 95;
+  coinX = random(width);
+  coinY = random(height);
+  coin2X = random(width);
+  coin2Y = random(height);
+  time = 500;
+  gameIsOver = false;
+  score = 0;
+  coinW2 = 20;
+  coinW = 20;
+  addTimeC = 20;
+  aTCX = width/2;
+  aTCY = height/2;
 }
 
-function draw(){
-  background(220);
-
-  // if the logo hits border, change directions.
-  // if (x >= (width - logoWidth) || x <= 0){
-  //   xVelocity = xVelocity * -1;
-  //   // xVelocity = -xVelocity;
-  // }
-  // // if the logo hits border, change directions.
-  // if (y >= (height - logoHeight) || y<= 0){
-  //   yVelocity = yVelocity * -1;
-  // }
-  if (y < height){
-    y += yVelocity;
+function draw() {
+  background(backgroundColor);
+  fill('white');
+  ellipse(coinX, coinY, 20);
+  fill('white')
+  ellipse(mouseX, mouseY, 20);
+  fill("black");
+  text(`Time remaining: ${time}`, 20, 40);
+  text(`Your score: ${score}`, 20, 20);
+  fill(330, 180, 222);
+  ellipse(coin2X, coin2Y, coinW2);
+  handleTime();
+  hit = collideCircleCircle(mouseX, mouseY, coinW, coinX, coinY, coinW);
+  hit2 = collideCircleCircle(mouseX, mouseY, coinW, coin2X, coin2Y, coinW2)
+  if (hit && !gameIsOver) {
+    handleCollision();
+  }
+  if(hit2 && !gameIsOver){
+    handleCollisionTwo();
   }
 
-  
-  x = x + xVelocity;
-  y = y + yVelocity;
-  // Draw the logo at the new position.
-  image(dvdImage, x, y, logoWidth, logoHeight);
 }
-function keyPressed(){
-   y-= 10; 
-   yVelocity = 0;
+
+function handleCollision() {
+  // We'll write code for what happens if your character hits a coin.
+  console.log(`You got a hit at ${mouseX}, ${mouseY}!`);
+
+  score++;
+  coinX = random(width);
+  coinY = random(height);
 }
-function keyReleased(){ 
-   yVelocity = 1;
+function handleCollisionTwo(){
+  console.log(`You got a hit at ${mouseX}, ${mouseY}!`);
+
+  score+= 5;
+  coin2X = random(width);
+  coin2Y = random(height);
+  if(coinW2> 0){
+    coinW2 -= 3;
+  }
+    
+}
+
+function handleTime() {
+  // We'll write code to handle the time.
+  if (time > 0) {
+    time--;
+  } else {
+    gameIsOver();
+  }
 }
