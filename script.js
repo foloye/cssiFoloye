33075cc4ac69ce164aa606bccde1950cc6c8427a
Glitch (cssi-day-5-project-snake) <none>
2020-10-01 05:28:28 +0000

    🚴🌍 Updated with Glitch

--- script.js
+++ script.js
@@ -1,104 +1,83 @@
-/* global createCanvas,rect, colorMode, HSB, noStroke, color, background, fill, ellipse, text, stroke, line, width, height, mouseX, mouseY */
-
-// We'll use variables for most of our colors in this code-along.
-let backgroundColor, square, color1, color2,colorRect1,colorRect2,colorRect3,colorRect4, textColor, globalS, globalB;
+/* global createCanvas,snakeImage, snakeY, background, loadImage, image, width, height, translate, rect, ellipse, fill, noStroke, mouseX, mouseY, text, color */
+let x,
+  y,
+  yVelocity,
+  obstacleH,
+  obstacleW,
+  snakeX,
+  snakeY,
+  snakeImage,
+  gameOn,
+  x2;
 
 function setup() {
-  // Canvas & color settings
-  createCanvas(800, 800);
-  colorMode(HSB, 360, 100, 100);
+  createCanvas(720, 400);
   noStroke();
-  globalS = 50;
-  globalB = 100;
-  square = 50;
-
-  // When used with only one argument, the color mode is greyscale.
-  // 0 is black and 100 is white.
-  backgroundColor = color(95);
-  textColor = color(20);
-  // When used with three arguments, the function takes, in this order:
-  // HUE - 0 to 360 degrees on a color wheel - 0 is red, 120 is green and 240
-  //       is blue.
-  // SATURATION - 0 is no color (greyscale), and 100 is as bold as possible.
-  // BRIGHTNESS - 0 is no light (black), and 100 is as bright as possible.
-  color1 = color(0, globalS, globalB);
-  color2 = color(200, globalS, globalB);
-  colorRect1 = color(55, globalS, globalB);
-  colorRect2 = color(160, globalS, globalB);
-  colorRect3 = color(330, globalS, globalB);
-  colorRect4 = color(200, globalS, globalB);
+  snakeX = 40;
+  snakeY = height / 4;
+  snakeImage = loadImage("https://i.ibb.co/yFS6Pz4/images.png");
+  x = 0;
+  x2 = 20;
+  y = 0;
+  obstacleH = 50;
+  obstacleW = 50;
+  gameOn = true;
+  yVelocity = 1;
 }
 
 function draw() {
-  background(backgroundColor);
-  // Call the drawCenterLine function here to run the three lines of code
-  // contained in that function.
-  drawCenterLine();
-
-  /*if (mouseX > width / 2) {
-    nightMode();
-  } 
-  else {
-    dayMode();
-  }
-  */
-  if (mouseX < square && mouseY > square){
-    backgroundColor = color(colorRect1);
-  }
-  else if (mouseX < (width - square) && mouseY > square){
-    backgroundColor = color(colorRect2);
-  }
-  else if (mouseX < square && mouseY > 350){
-    backgroundColor = color(colorRect3);
-  }
-  else if (mouseX > (width - square) && mouseY > (height - square)){
-    backgroundColor = color(colorRect4);
-  }
+  background(102);
+  // snake avatar
+  fill(150, 250, 150);
+  ellipse(snakeX, snakeY, 40, 40);
+  // image(snakeImage, snakeX, snakeY);
+  // Obstacle 1
 
 
-  // The red and blue circles:
-  fill(color1);
-  ellipse(width / 4, height / 2, 50);
-  fill(color2);
-  ellipse(width * (3 / 4), height / 2, 50);
+  // Animate by increasing our x value
+  x = x + 0.8;
+  // If the shape goes off the canvas, reset the position
+  if (x > width) {
+    x = -obstacleW;
+  }
+
+  translate(x, 0);
+  // upper
+  fill(0);
+  rect(0, 0, obstacleW, obstacleH);
+  // lower
+  fill(255);
+  rect(0, height - obstacleW, obstacleW, 2*obstacleH);
+  fill(255);
   
-  
-  //drawing rectangles
-  fill(colorRect1);
-  rect(0,0, square, square);
-  fill(colorRect2);
-  rect(width-50,0, square, square);
-  fill(colorRect3);
-  rect(0,height-50,square, square);
-  fill(colorRect4);
-  rect(width-50, height-50, square, square);
 
-  // The grey circle and the text:
-  fill(textColor);
-  ellipse(mouseX, mouseY, 50);
-  text("Flip the switch (" + mouseX + ", " + mouseY + ")", 20, 20);
+  if (snakeY < height) {
+    snakeY += yVelocity;
+  }
+  if (snakeY >= height || snakeX >= width){
+    background(0);
+  }
+
+
 }
 
-function drawCenterLine() {
-  // This function will turn stroke on, draw the line, and then turn stroke
-  // back off.
-  // Remember a line segment in p5.js has four arguments: x1, y1, x2, y2
-  stroke(textColor);
-  line(width / 2, 0, width / 2, height);
-  noStroke();
+function keyPressed() {
+  snakeY -= 10;
+  snakeX += 20;
+  yVelocity = 0;
 }
-
-function nightMode() {
-  backgroundColor = color(20);
-  textColor = color(95);
-  color1 = color(200, globalS, globalB);
-  color2 = color(0, globalS, globalB);
+function keyReleased() {
+  yVelocity = 1;
 }
-
-function dayMode() {
-  backgroundColor = color(95);
-  textColor = color(20);
-  color1 = color(0, globalS, globalB);
-  color2 = color(200, globalS, globalB);
+// function mouseReleased(){
+//    snakeY-= 5;
+// }
+function mousePressed() {
+  snakeY -= 10;
+  snakeX += 20;
+  yVelocity = 0;
 }
-
+function mouseReleased() {
+  yVelocity = 1;
+}
+//image(snakeImage, snakeX, snakeY, 50,50)
--- solutions.js
+++ solutions.js
@@ -1,71 +1,40 @@
-/* global createCanvas, colorMode, HSB, noStroke, color, background, fill, ellipse, text, stroke, line, width, height, mouseX, mouseY */
+/* global createCanvas, background, loadImage, image */
 
-let backgroundColor, color1, color2, textColor, globalS, globalB;
+let dvdImage, x, xVelocity, y, yVelocity, canvasWidth, canvasHeight, logoWidth, logoHeight;
 
-function setup() {
-  globalS = 100;
-  globalB = 50;
+function setup(){
+  canvasWidth = 800;
+  canvasHeight = 600;
+  logoWidth = 200;
+  logoHeight = 150;
   
-  // Canvas & color settings
-  createCanvas(800, 800);
-  colorMode(HSB, 360, 100, 100);
-  noStroke();
-
-  // When used with only one argument, the color mode is greyscale.
-  // 0 is black and 100 is white.
-  backgroundColor = color(95);
-  textColor = color(20);
-  // When used with three arguments, the function takes, in this order:
-  // HUE - 0 to 360 degrees on a color wheel - 0 is red, 120 is green and 240
-  //       is blue.
-  // SATURATION - 0 is no color (greyscale), and 100 is as bold as possible.
-  // BRIGHTNESS - 0 is no light (black), and 100 is as bright as possible.
-  color1 = color(0, globalS, globalB);
-  color2 = color(200, globalS, globalB);
+  createCanvas(canvasWidth, canvasHeight);
+  // We only want to load the logo once.
+  dvdImage = loadImage("https://cdn.glitch.com/eaea72a4-ac6d-4777-b76e-f37d75959aa5%2Fdvd.jpeg?1515761833387");
+  
+  // Set up starting values.
+  x = 50;
+  xVelocity = 5;
+  y = 50;
+  yVelocity = 5;
 }
 
-function draw() {
-  background(backgroundColor);
-  // Call the drawCenterLine function here to run the three lines of code
-  // contained in that function.
-  drawCenterLine();
-  if (mouseX > width / 2) {
-    nightMode();
-  } else {
-    dayMode();
+function draw(){
+  background(220);
+
+  x += xVelocity;
+  y += yVelocity;
+  
+  // If it hits a canvas border, switch direction.
+  if (x < 0 || x > canvasWidth - logoWidth) {
+    xVelocity = -1 * xVelocity;
   }
-
-  // The red and blue circles:
-  fill(color1);
-  ellipse(width * 1 / 4, height / 2, 50);
-  fill(color2);
-  ellipse(width * 3 / 4, height / 2, 50);
-
-  // The grey circle and the text:
-  fill(textColor);
-  ellipse(mouseX, mouseY, 50);
-  text("Flip the switch (" + mouseX + ", " + mouseY + ")", 20, 20);
-}
-
-function drawCenterLine() {
-  // This function will turn stroke on, draw the line, and then turn stroke
-  // back off.
-  // Remember a line segment in p5.js has four arguments: x1, y1, x2, y2
-  stroke(textColor);
-  line(width / 2, 0, width / 2, height);
-  noStroke();
-}
-
-function nightMode() {
-  backgroundColor = color(20);
-  color1 = color(200, globalS, globalB);
-  color2 = color(0, globalS, globalB);
-  textColor = color(95);
-}
-
-function dayMode() {
-  backgroundColor = color(95);
-  color1 = color(0, globalS, globalB);
-  color2 = color(200, globalS, globalB);
-  textColor = color(20);
+  
+  // If it hits a canvas border, switch direction.
+  if (y < 0 || y > canvasHeight - logoHeight) {
+    yVelocity = -1 * yVelocity;
+  }
+  
+  // Draw the logo at the new position.
+  image(dvdImage, x, y, logoWidth, logoHeight);
 }
