75fd4a6fc366f8e4c06851a7bd2b1a5b30619f5f
Glitch (powerful-coral-sweatshirt) <none>
2020-10-01 05:28:00 +0000

    🕌💈 Updated with Glitch

--- script.js
+++ script.js
@@ -1,80 +1,177 @@
 // Be sure to name any p5.js functions we use in the global so Glitch can recognize them.
 // Add to this list as you consult the p5.js documentation for other functions.
-/* global createCanvas, colorMode, HSB, width, height, random, background, fill, color, random,
-          rect, ellipse, stroke, image, loadImage, collideCircleCircle, collideRectCircle, text, 
+/* global createCanvas, colorMode, HSB, width, height, random, round, background, fill, noFill, color, random,
+          rect, ellipse, stroke, image, loadImage, frameRate, collideRectRect, collideRectCircle, text, 
           mouseX, mouseY, strokeWeight, line, mouseIsPressed, windowWidth, windowHeight, noStroke, 
-          keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize */
+          keyCode, UP_ARROW, LEFT_ARROW, RIGHT_ARROW, DOWN_ARROW, textSize, noLoop, loop */
 
-let brushHue,addTimeC,aTCX,aTCY, backgroundColor,coinW, coin2X,coin2Y,coinW2, coinX, coinY, score, time, gameIsOver, hit, hit2;
+let backgroundColor, playerSnake, currentApple, score;
 
 function setup() {
   // Canvas & color settings
   createCanvas(400, 400);
   colorMode(HSB, 360, 100, 100);
-  brushHue = 0;
   backgroundColor = 95;
-  coinX = random(width);
-  coinY = random(height);
-  coin2X = random(width);
-  coin2Y = random(height);
-  time = 500;
-  gameIsOver = false;
+  frameRate(12);
+  playerSnake = new Snake();
+  currentApple = new Apple();
   score = 0;
-  coinW2 = 20;
-  coinW = 20;
-  addTimeC = 20;
-  aTCX = width/2;
-  aTCY = height/2;
 }
 
 function draw() {
   background(backgroundColor);
-  fill('white');
-  ellipse(coinX, coinY, 20);
-  fill('white')
-  ellipse(mouseX, mouseY, 20);
-  fill("black");
-  text(`Time remaining: ${time}`, 20, 40);
-  text(`Your score: ${score}`, 20, 20);
-  fill(330, 180, 222);
-  ellipse(coin2X, coin2Y, coinW2);
-  handleTime();
-  hit = collideCircleCircle(mouseX, mouseY, coinW, coinX, coinY, coinW);
-  hit2 = collideCircleCircle(mouseX, mouseY, coinW, coin2X, coin2Y, coinW2)
-  if (hit && !gameIsOver) {
-    handleCollision();
-  }
-  if(hit2 && !gameIsOver){
-    handleCollisionTwo();
-  }
-
+  // The snake performs the following four methods:
+  playerSnake.moveSelf();
+  playerSnake.showSelf();
+  playerSnake.checkCollisions();
+  playerSnake.checkApples();
+  // The apple needs fewer methods to show up on screen.
+  currentApple.showSelf();
+  // We put the score in its own function for readability.
+  displayScore();
 }
 
-function handleCollision() {
-  // We'll write code for what happens if your character hits a coin.
-  console.log(`You got a hit at ${mouseX}, ${mouseY}!`);
-
-  score++;
-  coinX = random(width);
-  coinY = random(height);
+function displayScore() {
+  fill(0);
+  text(`Score: ${score}`, 20, 20);
 }
-function handleCollisionTwo(){
-  console.log(`You got a hit at ${mouseX}, ${mouseY}!`);
 
-  score+= 5;
-  coin2X = random(width);
-  coin2Y = random(height);
-  if(coinW2> 0){
-    coinW2 -= 3;
+class Snake {
+  constructor() {
+    this.size = 10;
+    this.x = width / 2;
+    this.y = height - 10;
+    this.direction = "N";
+    this.speed = 12;
+    this.tail = [];
+    this.tail.unshift(new TailSegment(this.x, this.y));
   }
-    
+
+  moveSelf() {
+    if (this.direction === "N") {
+      this.y -= this.speed;
+    } else if (this.direction === "S") {
+      this.y += this.speed;
+    } else if (this.direction === "E") {
+      this.x += this.speed;
+    } else if (this.direction === "W") {
+      this.x -= this.speed;
+    } else {
+      console.log("Error: invalid direction");
+    }
+    this.tail.unshift(new TailSegment(this.x, this.y));
+    this.tail.pop();
+  }
+
+  showSelf() {
+    stroke(240, 100, 100);
+    noFill();
+    rect(this.x, this.y, this.size, this.size);
+    noStroke();
+    for (let i = 0; i < this.tail.length; i++) {
+      this.tail[i].showSelf();
+    }
+  }
+
+  checkApples() {
+    // If the head of the snake collides with the apple...
+    if (
+      collideRectRect(
+        this.x,
+        this.y,
+        this.size,
+        this.size,
+        currentApple.x,
+        currentApple.y,
+        currentApple.size,
+        currentApple.size
+      )
+    ) {
+      // Make a new apple, increment the score, and extend the tail.
+      score += 1;
+      currentApple = new Apple();
+      this.extendTail();
+    }
+  }
+
+  checkCollisions() {
+    // If there is only one tail segment, no collision
+    if (this.tail.length <= 2) {
+      return;
+    }
+    for (let i = 1; i < this.tail.length; i++) {
+      if (this.x == this.tail[i].x && this.y == this.tail[i].y) {
+        gameOver();
+      }
+    }
+  }
+
+  extendTail() {
+    // Add a new segment at the end of the tail.
+    let lastTailSegment = this.tail[this.tail.length - 1];
+    // Push a new tail segment to the end of the tail array.
+    this.tail.push(new TailSegment(lastTailSegment.x, lastTailSegment.y));
+  }
 }
 
-function handleTime() {
-  // We'll write code to handle the time.
-  if (time > 0) {
-    time--;
+class TailSegment {
+  constructor(x, y) {
+    this.size = 10;
+    this.x = x;
+    this.y = y;
+  }
+
+  showSelf() {
+    fill(0);
+    rect(this.x, this.y, this.size, this.size);
+  }
+}
+
+class Apple {
+  constructor() {
+    this.x = round(random(width - 10));
+    this.y = round(random(height - 10));
+    this.size = 10;
+  }
+
+  showSelf() {
+    stroke(240, 100, 100);
+    noFill();
+    rect(this.x, this.y, this.size, this.size);
+    noStroke();
+  }
+}
+
+function keyPressed() {
+  console.log("key pressed: ", keyCode);
+  if (keyCode === UP_ARROW && playerSnake.direction != "S") {
+    playerSnake.direction = "N";
+  } else if (keyCode === DOWN_ARROW && playerSnake.direction != "N") {
+    playerSnake.direction = "S";
+  } else if (keyCode === RIGHT_ARROW && playerSnake.direction != "W") {
+    playerSnake.direction = "E";
+  } else if (keyCode === LEFT_ARROW && playerSnake.direction != "E") {
+    playerSnake.direction = "W";
+  } else if (keyCode === 32) {
+    restartGame();
   } else {
-    gameIsOver();
+    console.log("wrong key");
   }
 }
+
+function restartGame() {
+  score = 0;
+  playerSnake = new Snake();
+  currentApple = new Apple();
+  loop();
+}
+
+function gameOver() {
+  console.log("GAME OVER!");
+  stroke(0);
+  text("GAME OVER", 50, 50);
+  noLoop();
+}
+
+let resetColor ;
+
